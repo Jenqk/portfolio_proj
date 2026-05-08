d2f985d8db6e84b831352ce6aaf70704f5ddec23
Jenqk <jenqk24@gmail.com>
2026-05-08 09:26:46 +0300

    s

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   plugins: [react(), tailwindcss(), viteSingleFile()],
-  base: '/jenqk.github.io/portfolio_proj/',
+  base: '/jenqk.github.io/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
